93553156dbb25692aeb175af028a27ac03ee15b6
Jonathan L Long <jon@symbiorobotics.com>
2016-11-08 15:11:22 -0800

    don't save solverstate

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -421,7 +421,7 @@ void Solver<Dtype>::Snapshot() {
     LOG(FATAL) << "Unsupported snapshot format.";
   }
 
-  SnapshotSolverState(model_filename);
+  // SnapshotSolverState(model_filename);
 }
 
 template <typename Dtype>
